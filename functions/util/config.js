f1e6a91adae3d9898034ccbea90d712fee4a140a
leobuck <leobuck12@gmail.com>
2020-08-14 14:55:01 -0300

    first commit

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyCrf7SLkeetI3ocm8tsv8008YgBOOf6I_Q",
+    authDomain: "todoapp-8a6d8.firebaseapp.com",
+    databaseURL: "https://todoapp-8a6d8.firebaseio.com",
+    projectId: "todoapp-8a6d8",
+    storageBucket: "todoapp-8a6d8.appspot.com",
+    messagingSenderId: "459992986225",
+    appId: "1:459992986225:web:50a93a35692882804da1d0",
+    measurementId: "G-91Y7B3G9LJ"
+};
